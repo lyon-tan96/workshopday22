70b0c3c56345a586933d16fcfa788dca58f1a7d6
lyon-tan96 <lyontsy@gmail.com>
2022-04-05 11:07:21 +0800

    initial commit

---
+++ src/main/java/nus/iss/paf/workshopday22/Workshopday22Application.java
@@ -0,0 +1,13 @@
+package nus.iss.paf.workshopday22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopday22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopday22Application.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/paf/workshopday22/Workshopday22ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.paf.workshopday22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopday22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
